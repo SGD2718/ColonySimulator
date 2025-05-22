56f2513df9050c4ed68edc779d8c8d293a8d7da9
Gradient Descent <48599511+SGD2718@users.noreply.github.com>
2025-05-21 23:42:49 -0700

    set up

--- environment/light.py
+++ environment/light.py
@@ -1,5 +1,19 @@
-class Light:
-    def __init__(self, size, brightness, power_per_size_per_lumen):
+from subsystem import Subsystem
+from power_source import PowerSource
+
+
+class Light(Subsystem):
+    def __init__(self,
+                 name: str,
+                 source: PowerSource,
+                 dims: tuple[float, float, float],
+                 brightness,
+                 power_per_area_per_lumen,
+                 ):
+        super().__init__(name, source)
         self.brightness = brightness
-        self.size = size
-        self.power_rate = size * power_per_size_per_lumen * brightness
+
+
+    def update(self, dt: float = 0.0333) -> float:
+        used_energy = self.power_source.consume_electricity(power * dt)
+        pass
